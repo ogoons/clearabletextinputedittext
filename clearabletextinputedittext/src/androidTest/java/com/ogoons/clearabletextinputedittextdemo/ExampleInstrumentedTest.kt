fc37ef0007f6ae07e90c4125ff878d15c5852a21
Charles.Oh <charles.oh@kinemaster.com>
2021-08-30 13:12:39 +0900

    Change name of packages 3

--- clearabletextinputedittext/src/androidTest/java/com/ogoons/clearabletextinputedittextdemo/ExampleInstrumentedTest.kt
+++ clearabletextinputedittext/src/androidTest/java/com/ogoons/clearabletextinputedittextdemo/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.ogoons.clearabletextinputedittext.test", appContext.packageName)
+        assertEquals("com.ogoons.clearabletextinputedittextdemo.test", appContext.packageName)
     }
 }
